6e0184bd4ea78c794b4527ae4d0a4834478cf715
Nick Machnik <nick.machnik@gmail.com>
2020-06-11 16:35:34 +0200

    test for subset command

--- tests/cli.rs
+++ tests/cli.rs
@@ -39,7 +39,7 @@ fn test_get_entry_cmd_success_uniprot() -> Result<(), Box<dyn std::error::Error>
     let mut cmd = Command::cargo_bin("fastatools")?;
     cmd.arg("get-entry")
         .arg("resources/test.fasta")
-        .arg("resources/test.index")
+        .arg("resources/test_index.json")
         .arg("H0VS30");
     let descr = "tr|H0VS30|H0VS30_CAVPO Receptor protein serine/threonine kinase OS=Cavia porcellus OX=10141 GN=TGFBR1 PE=3 SV=2";
     let seq = "MEAAAAAPRHQLLLLMLVAAAATLLPGAKALQCFCQLCAKDNYTCVTDGLCFVSITETTDRIIHNTMCIAEIDLIPRDRPFVCAPSSKTGAVTTTHCCNQDHCNKIELPTTEKQS\
@@ -49,6 +49,39 @@ fn test_get_entry_cmd_success_uniprot() -> Result<(), Box<dyn std::error::Error>
     QQEGIKMSFVVCH";
     cmd.assert().stdout(predicate::str::contains(descr));
     cmd.assert().stdout(predicate::str::contains(seq));
+    Ok(())
+}
+
+#[test]
+fn test_subset_cmd_success_uniprot() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("fastatools")?;
+    cmd.arg("subset")
+        .arg("resources/test.fasta")
+        .arg("resources/test_index.json")
+        .arg("resources/subset_ids.txt");
+    let descr_a = "tr|H0VS30|H0VS30_CAVPO Receptor protein serine/threonine kinase OS=Cavia porcellus OX=10141 GN=TGFBR1 PE=3 SV=2";
+    let seq_a = "MEAAAAAPRHQLLLLMLVAAAATLLPGAKALQCFCQLCAKDNYTCVTDGLCFVSITETTD\
+    RIIHNTMCIAEIDLIPRDRPFVCAPSSKTGAVTTTHCCNQDHCNKIELPTTEKQSSGLGP\
+    VELAAVIAGPVCFVCISLMLMVYICHNRTVIHHRVPNEEDPSLDRPFISEGTTLKDLIYD\
+    MTTSGSGSGLPLLVQRTIARTIVLQESIGKGRFGEVWRGKWRGEEVAVKIFSSREERSWF\
+    REAEIYQTVMLRHENILGFIAADNKDNGTWTQLWLVSDYHEHGSLFDYLNRYTVTVEGMI\
+    KLALSTASGLAHLHMEIVGTQGKPAIAHRDLKSKNILVKKNGTCCIADLGLAVRHDSATD\
+    TIDIAPNHRVGTKRYMAPEVLDDSINMKHFESFKRADIYAMGLVFWEIARRCSIGGIHED\
+    YQLPYYDLVPSDPSVEEMRKVVCEQKLRPNIPNRWQSCEALRVMAKIMRECWYANGAARL\
+    TALRIKKTLSQLSQQEGIKMSFVVCH";
+    let descr_b = "tr|Q8I5U1|Q8I5U1_PLAF7 GPI mannosyltransferase 1 OS=Plasmodium falciparum (isolate 3D7) OX=36329 GN=PF3D7_1210900 PE=3 SV=2";
+    let seq_b = "MGHTHKEYKNNEKSAIFFEWLIFFVGIIIRIIIYYYGRWQDKNFNVKFTDVDYYVFSDAA\
+    KYVLMNKSPYERYTYRYTPLLAYIMIPNFFVHFSFGKILFSFIDILVTILINQIIKIKYT\
+    NCKNYIFYTCLWFLNPLVIIISLRGNADVIPCFLIIVTIFCIYKKHIFLSSIFYGLAVNF\
+    KIYTIIYALPFMLYLNKNYLLGENIFQLNEKKKKKKNDFLLNTFFYIFRIISNFFVELFK\
+    LNYEQFLFAICSSSVFLILNCVFYIIYGYEFLYESFIYHIIRRDHRHNFSLFFYLMYLSI\
+    EKNSKIIPLITFVPQIILVALFGFKYARTNLELSMFLQTISFIALNKVCTSQYFIWCIPF\
+    LPIILCAITLSKRNMFLIISSILFFIVAKLHWLWWAYYLEFRGYNTFLQLFYSSVLFVIS\
+    EISICWVFMYINFKEQKTKIT";
+    cmd.assert().stdout(predicate::str::contains(descr_a));
+    cmd.assert().stdout(predicate::str::contains(seq_a));
+    cmd.assert().stdout(predicate::str::contains(descr_b));
+    cmd.assert().stdout(predicate::str::contains(seq_b));
     Ok(())
 }
 
